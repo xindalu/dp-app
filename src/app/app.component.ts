cb5488ab33efcfa53099ad8addfea46d005c3d26
xindalu <14706931@qq.com>
2018-03-29 14:27:09 +0800

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import {Component, OnInit} from '@angular/core';
 
+import { UserService } from "./shared/services/user.service";
+import { StorageService } from "./shared/services/storage.service";
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.css'],
+    providers: [UserService, StorageService]
 })
 export class AppComponent implements OnInit{
+    userLogined: boolean = false;
+
+    constructor(
+        private userService: UserService
+    ) {}
 
     ngOnInit() {
+        this.userLogined = this.userService.isLogined();
     }
 }
